541a8df5cd7e777037bfa07e81f4dde6a92d3c30
David Huck <dnhuck@dmacc.edu>
2021-03-04 11:32:17 -0600

    added dynamic header/footer and images

---
+++ js/footer.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+    $('.footer').html(`
+        <div class = "navFooter">
+            <ul>
+                <a href = "#"><li>Home</li></a>
+                <a href = "about.html"><li>About</li></a>
+                <a href = "floorPlans.html"><li>Floor Plans</li></a>
+                <a href = "contact.html"><li>Contact</li></a>
+            </ul>
+        </div>
+
+        <div class = "address">
+            <h6>Vintage Cooperative Of Prairie Trail</h6>
+            <address>1370 SW Magazine Road<br>
+            Ankeny, IA 50023<br>
+            515-729-2040<br>
+            ptoffice@vintagecooperatives.com</address>
+        </div>
+    `);
+})
---
+++ js/nav.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+    $('.nav').html(`
+    
+        <ul>
+            <a href = "index.html"><li>Home</li></a>
+            <a href = "about.html"><li>About</li></a>
+            <a href = "floorPlans.html"><li>Floor Plans</li></a>
+            <a href = "gallery.html"><li>Gallery</li></a>
+            <a href = "contact.html"><li>Contact</li></a>
+        </ul>
+    
+    `);
+})
