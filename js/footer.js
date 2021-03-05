887c8aa78794ff8ac64354704925990a83e0ad4d
Erica Manning <60369190+ejmanning@users.noreply.github.com>
2021-03-05 17:55:15 -0600

    Styles CSS and images

---
+++ js/footer.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+    $('.footer').html(`
+        <div class="row">
+            <div class="column">
+                <div class = "navFooter">
+                    <ul>
+                    <a href = "index.html"><li>Home</li></a>
+                    <a href = "about.html"><li>About</li></a>
+                    <a href = "floorPlans.html"><li>Floor Plans</li></a>
+                    <a href = "contact.html"><li>Contact</li></a>
+                    </ul>
+                </div>
+            </div>
+            <div class="column">
+                <div class = "address">
+                    <address>Vintage Cooperative Of Prairie Trail
+                    1370 SW Magazine Road<br>
+                    Ankeny, IA 50023<br>
+                    <a href="tel: 515-729-2040">515-729-2040</a><br>
+                    <a href="mailto: ptoffice@vintagecooperatives.com">ptoffice@vintagecooperatives.com</a></address>
+                </div>
+            </div>
+
+            <div class="column">
+                <p>logo goes here</p>
+            </div>
+
+        </div>
+    `);
+})
---
+++ js/nav.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.nav').html(`
+        <div class="navRow">
+            <div class="navColumn">
+                <img src="images/logo.png" alt="logo">
+            </div>
+            <div class="navColumn">
+                <div class="toggle">
+                    <i class="fas fa-bars mobileMenu" area-hidden="true"></i>
+                </div>
+                <ul>
+                    <li><a href = "index.html">Home</a></li>
+                    <li><a href = "about.html">About</a></li>
+                    <li><a href = "floorPlans.html">Floor Plans</a></li>
+                    <li><a href = "gallery.html">Gallery</a></li>
+                    <li><a href = "contact.html">Contact</a></li>
+                </ul>
+            </div>
+       </div>
+    `);
+
+     // COLLAPSEABLE MENU
+     $('.mobileMenu').click(function(){
+        $('ul').toggleClass('active');
+    });
+})
